ecd4ca2c6bb6929903477bb483fde8e373b052a8
GuiCeara <guilhermecororido@gmail.com>
2023-03-16 06:06:02 -0300

    15/03

--- components/cubo/CAC.php
+++ components/cubo/CAC.php
@@ -1,1 +1,23 @@
-<h1>TESTE</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href=".\CSS\style.css">
+</head>
+<body>
+
+    <div class="container CAC">
+        <h1>Calcular Área do Cubo</h1>
+        <form method="get">
+            <input type="number" name="val1" class="inputs">
+            <br>
+            <input type="number" name="val2" class="inputs">
+            <br>
+            <button>Calcular</button>
+        </form>
+        
+    </div>
+    
+    <!-- <img src=".\imagens\esfera.jpg" class="img_esfera">  -->
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -25,7 +26,7 @@
 <body>
   <form class="navbar navbar-expand-lg navbar-light bg-light" method="get">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Cálculos</a>
+      <a class="navbar-brand" href="#" id="span">Cálculos</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -37,7 +38,7 @@
               Cubo
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><button class="dropdown-item" name="areaC">Área</button></li>
+              <li><button class="dropdown-item" name="areaC" id="btn_area_cubo">Área</button></li>
               <li><button class="dropdown-item" name="volumeC">Volume</button></li>
             </ul>
           </li>
@@ -85,10 +86,11 @@
 
   ?>
 
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
   </script>
+
+
 
 </body>
 
